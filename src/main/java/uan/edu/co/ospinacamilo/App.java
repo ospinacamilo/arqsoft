52e701ce7463700636c40e1819ec19136ed76836
camiloospina <camiloospina@192.168.0.4>
2018-05-08 00:16:26 -0500

    Last version

--- src/main/java/uan/edu/co/ospinacamilo/App.java
+++ src/main/java/uan/edu/co/ospinacamilo/App.java
@@ -1,13 +1,13 @@
 package uan.edu.co.ospinacamilo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Hello world!
- *
- */
+import uan.edu.co.ospinacamilo.property.manager.PropertyApplication;
+
+@SpringBootApplication
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	public static void main(String[] args) {
+        SpringApplication.run(PropertyApplication.class, args);
     }
 }
